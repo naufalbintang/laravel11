9483d554cf1f46c4ec068e13b64b7e75c371c9c9
naufalbintang <naufalbintangpradana@gmail.com>
2026-01-04 19:19:41 +0700

    episode 3

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Naufal Bintang']);
+});
+
+
+//soal
+// buat 2 rute baru
+// 1. /blog
+// 2 buah artikel, judul dan isi
+
+// 2. /contact
+// email/social media
