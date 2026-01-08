3ef10ae9d34353504de9d1ce37f2739f98ba9a5f
naufalbintang <naufalbintangpradana@gmail.com>
2026-01-08 15:09:09 +0700

    episode 6

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">Welcome To My Blog</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,20 +11,61 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Naufal Bintang', "title" => "About"]);
 });
 
-//jawaban soal 1
-Route::get('/blog', function () {
-    return view('blog', ["title" => "Blog"]);
+Route::get('/posts', function () {
+    return view('posts', [
+        "title" => "Blog",
+        "posts" => [
+            [
+                "id" => 1,
+                "slug" => "judul-artikel-1",
+                "title" => "Judul Artikel 1",
+                "author" => "Naufal Bintang",
+                "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti officiis error sequi beatae, suscipit
+                perspiciatis hic necessitatibus omnis molestiae voluptatibus exercitationem repellendus sit earum est
+                doloribus? Laboriosam nam repellat aut?"
+            ],
+            [
+                "id" => 2,
+                "title" => "Judul Artikel 2",
+                "slug" => "judul-artikel-2",
+                "author" => "Naufal Bintang",
+                "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque fuga reiciendis repellendus eaque.
+                Quo, consectetur modi excepturi suscipit exercitationem quibusdam vitae neque aliquid nihil pariatur quas,
+                esse, corrupti beatae."
+            ]
+        ],
+    ]);
+});
+
+Route::get("/posts/{slug}", function ($slug) {
+    $posts = [
+        [
+            "id" => 1,
+            "slug" => "judul-artikel-1",
+            "title" => "Judul Artikel 1",
+            "author" => "Naufal Bintang",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti officiis error sequi beatae, suscipit
+                perspiciatis hic necessitatibus omnis molestiae voluptatibus exercitationem repellendus sit earum est
+                doloribus? Laboriosam nam repellat aut?"
+        ],
+        [
+            "id" => 2,
+            "slug" => "judul-artikel-2",
+            "title" => "Judul Artikel 2",
+            "author" => "Naufal Bintang",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque fuga reiciendis repellendus eaque.
+                Quo, consectetur modi excepturi suscipit exercitationem quibusdam vitae neque aliquid nihil pariatur quas,
+                esse, corrupti beatae."
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 
 Route::get('/contact', function () {
     return view('contact', ["title" => "Contact"]);
 });
-
-//soal 1
-// buat 2 rute baru
-// 1. /blog
-// 2 buah artikel, judul dan isi
-
-// 2. /contact
-// email/social media
