768c3509026d22484bafa47b66b01f798bcc7f37
naufalbintang <naufalbintangpradana@gmail.com>
2026-01-13 14:35:43 +0700

    episode 7

---
+++ app/Models/Post.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Post
+{
+    public static function all()
+    {
+        return [
+            [
+                "id" => 1,
+                "slug" => "judul-artikel-1",
+                "title" => "Judul Artikel 1",
+                "author" => "Naufal Bintang",
+                "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti officiis error sequi beatae, suscipit
+                perspiciatis hic necessitatibus omnis molestiae voluptatibus exercitationem repellendus sit earum est
+                doloribus? Laboriosam nam repellat aut?"
+            ],
+            [
+                "id" => 2,
+                "title" => "Judul Artikel 2",
+                "slug" => "judul-artikel-2",
+                "author" => "Naufal Bintang",
+                "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque fuga reiciendis repellendus eaque.
+                Quo, consectetur modi excepturi suscipit exercitationem quibusdam vitae neque aliquid nihil pariatur quas,
+                esse, corrupti beatae."
+            ]
+        ];
+    }
+
+    public static function find($slug): array
+    {
+        // return Arr::first(static::all(), function ($post) use ($slug) {
+        //     return $post["slug"] == $slug;
+        // });
+
+        $post = Arr::first(static::all(), fn($post) => $post["slug"] == $slug);
+
+        if (!$post) {
+            abort(404);
+        }
+        return $post;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('home', ["title" => "Home Page"]);
@@ -14,54 +17,14 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', [
         "title" => "Blog",
-        "posts" => [
-            [
-                "id" => 1,
-                "slug" => "judul-artikel-1",
-                "title" => "Judul Artikel 1",
-                "author" => "Naufal Bintang",
-                "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti officiis error sequi beatae, suscipit
-                perspiciatis hic necessitatibus omnis molestiae voluptatibus exercitationem repellendus sit earum est
-                doloribus? Laboriosam nam repellat aut?"
-            ],
-            [
-                "id" => 2,
-                "title" => "Judul Artikel 2",
-                "slug" => "judul-artikel-2",
-                "author" => "Naufal Bintang",
-                "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque fuga reiciendis repellendus eaque.
-                Quo, consectetur modi excepturi suscipit exercitationem quibusdam vitae neque aliquid nihil pariatur quas,
-                esse, corrupti beatae."
-            ]
-        ],
+        "posts" => Post::all(),
     ]);
 });
 
 Route::get("/posts/{slug}", function ($slug) {
-    $posts = [
-        [
-            "id" => 1,
-            "slug" => "judul-artikel-1",
-            "title" => "Judul Artikel 1",
-            "author" => "Naufal Bintang",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti officiis error sequi beatae, suscipit
-                perspiciatis hic necessitatibus omnis molestiae voluptatibus exercitationem repellendus sit earum est
-                doloribus? Laboriosam nam repellat aut?"
-        ],
-        [
-            "id" => 2,
-            "slug" => "judul-artikel-2",
-            "title" => "Judul Artikel 2",
-            "author" => "Naufal Bintang",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque fuga reiciendis repellendus eaque.
-                Quo, consectetur modi excepturi suscipit exercitationem quibusdam vitae neque aliquid nihil pariatur quas,
-                esse, corrupti beatae."
-        ]
-    ];
 
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+
+    $post = Post::find($slug);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
