489df9457dc5d42d719cbdb38fa3760b0f947bd5
naufalbintang <naufalbintangpradana@gmail.com>
2026-02-02 16:52:03 +0700

    episode 14

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
     return view('posts', [
         "title" => "Blog",
-        "posts" => Post::all(),
+        "posts" => $posts,
     ]);
 });
 
@@ -28,10 +30,12 @@ Route::get("/posts/{post:slug}", function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('category', 'author');
     return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load('category', 'author');
     return view('posts', ['title' => 'Articles in ' . $category->name, 'posts' => $category->posts]);
 });
 
